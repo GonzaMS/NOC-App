0f8a947daa1f6ae5f52a274702c19f9c81f35763
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-12-13 15:35:45 -0300

    Fixing/Adding new envs

--- src/config/envs.plugin.ts
+++ src/config/envs.plugin.ts
@@ -14,4 +14,10 @@ export const envs = {
   MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
   MONGO_USER: env.get("MONGO_URL").required().asString(),
   MONGO_PASS: env.get("MONGO_URL").required().asString(),
+
+  // Postgresql config
+  POSTGRES_URL: env.get("POSTGRES_URL").required().asString(),
+  POSTGRES_DB_NAME: env.get("POSTGRES_DB_NAME").required().asString(),
+  POSTGRES_USER: env.get("POSTGRES_USER").required().asString(),
+  POSTGRES_PASS: env.get("POSTGRES_PASS").required().asString(),
 };
